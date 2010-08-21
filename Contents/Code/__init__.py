b21bfec32c4e6ce210abc64d42152d1eea6744d0
superpea <scott@plexapp.com>
2010-08-21 11:58:29 -0700

    simplify a bit, fix a bug in art gathering.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,9 +1,9 @@
 #local media assets agent
 import os
 
-artExt = ['jpg','jpeg','png','tbn']
-posterFiles = ['poster','default','cover','movie']
-fanartFiles = ['fanart']
+artExt =   ['jpg','jpeg','png','tbn']
+allFiles = {'posters': ['poster','default','cover','movie'],
+            'art':     ['fanart']}
 
 class localmedia(Agent.Movies):
   name = 'Local Media Assets'
@@ -18,12 +18,9 @@ class localmedia(Agent.Movies):
     
   def update(self, metadata, media, lang):
     filename = media.items[0].parts[0].file
-    #Log('Local Media Filename: ' + filename)
     path = os.path.dirname(filename)
-    Log(path)
     if 'video_ts' == path.lower().split('/')[-1]:
       path = '/'.join(path.split('/')[:-1])
-      Log(path)
     basename = os.path.basename(filename)
     (fileroot, ext) = os.path.splitext(basename)
     pathFiles = {}
@@ -31,25 +28,18 @@ class localmedia(Agent.Movies):
     for p in os.listdir(path):
       pathFiles[p.lower()] = p
       pathFilesLower += [p]
-    extraFiles = [fileroot, path.split('/')[-1]]
 
-    #look for posters
-    for e in artExt:
-      for a in posterFiles + extraFiles: # all posterfile names, the filename, the foldername (as a file)
-        f = (a + '.' + e).lower()
-        Log(f)
-        if f in pathFilesLower:
-          data = Core.storage.load(os.path.join(path, pathFiles[f]))
-          if f not in metadata.posters:
-            metadata.posters[f] = Proxy.Media(data)
-            Log('local poster added: ' + f)
-            
-    #look for fanart
-    for e in artExt:
-      for a in fanartFiles + extraFiles:
-        f = (a + '.' + e).lower()
-        if f in pathFilesLower:
-          data = Core.storage.load(os.path.join(path, pathFiles[f]))
-          if f not in metadata.art:
-            metadata.art[f] = Proxy.Media(data)
-            Log('local fanart added: ' + f)    
+    allFiles['posters'] = allFiles['posters'] + [fileroot, path.split('/')[-1]] #add the filename as a base, and the dirname as a base for poster lookups
+    
+    #look for posters and art
+    for t in ['posters','art']:
+      for e in artExt:
+        for a in allFiles[t]:
+          f = (a + '.' + e).lower()
+          if f in pathFilesLower:
+            data = Core.storage.load(os.path.join(path, pathFiles[f]))
+            if t == 'posters':
+              if f not in metadata.posters: metadata.posters[f] = Proxy.Media(data)
+            elif t == 'art':
+              if f not in metadata.art: metadata.art[f] = Proxy.Media(data)
+            Log('local asset (type: ' + t + ') added: ' + f)
