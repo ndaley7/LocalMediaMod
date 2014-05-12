32499ed81593b78d6665d3ab2071031eddcec3c5
mmccurdy07 <mmccurdy@gmail.com>
2014-05-12 12:53:06 -0700

    Strip out unicode control characters that can appear in tags, etc.

--- Contents/Code/helpers.py
+++ Contents/Code/helpers.py
@@ -1,5 +1,15 @@
 import unicodedata
 
+# Unicode control characters can appear in ID3v2 tags but are not legal in XML.
+RE_UNICODE_CONTROL =  u'([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
+                      u'|' + \
+                      u'([%s-%s][^%s-%s])|([^%s-%s][%s-%s])|([%s-%s]$)|(^[%s-%s])' % \
+                      (
+                        unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff),
+                        unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff),
+                        unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff)
+                      )
+
 # A platform independent way to split paths which might come in with different separators.
 def splitPath(str):
   if str.find('\\') != -1:
@@ -13,7 +23,10 @@ def unicodize(s):
     filename = unicodedata.normalize('NFC', unicode(s.decode('utf-8')))
   except: 
     Log('Failed to unicodize: ' + filename)
-    pass
+  try:
+    filename = re.sub(RE_UNICODE_CONTROL, '', filename)
+  except:
+    Log('Couldn\'t strip control characters: ' + filename)
   return filename
 
 def cleanFilename(filename):
