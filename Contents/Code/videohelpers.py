7850c6b94a45b307f49f2c9b1a3c5dd49ee5fd0e
sandman4sure <sanderbeekmans@me.com>
2015-07-07 22:45:28 +0200

    Only use iTunes style metadata when not empty
    Issue: https://github.com/plexinc-agents/LocalMedia.bundle/issues/56

--- Contents/Code/videohelpers.py
+++ Contents/Code/videohelpers.py
@@ -117,26 +117,47 @@ class MP4VideoHelper(VideoHelper):
     # Directors
     try:
       if pl and 'directors' in pl and pl['directors']:
-        item.directors.clear()
+        pl_directors = []
         for director in pl['directors']:
-          item.directors.add(director['name'])
+          director_name = director['name']
+          if director_name:
+            pl_directors.add(director_name)
+        # if there are none-empty director names present use them
+        if pl_directors:
+          item.directors.clear()
+          for director_name in pl_directors:
+            item.directors.add(director_name)
     except: pass
 
     # Writers
     try:
       if pl and 'screenwriters' in pl and pl['screenwriters']:
-        item.writers.clear()
+        pl_screenwriters = []
         for writer in pl['screenwriters']:
-          item.writers.add(writer['name'])
+          writer_name = writer['name']
+          if writer_name:
+            pl_screenwriters.add(writer_name)
+        # if there are none-empty writer names present use them
+        if pl_screenwriters:
+          item.writers.clear()
+          for writer_name in pl_screenwriters:
+            item.writers.add(writer_name)
     except: pass
 
     # Cast
     try:
+      pl_actors = []
       if pl and 'cast' in pl and pl['cast']:
-        item.roles.clear()
         for actor in pl['cast']:
+          actor_name = actor['name']
+          if actor_name:
+            pl_actors.add(actor_name)
+
+      if pl_actors:
+        item.roles.clear()
+        for actor_name in pl_actors:
           role = item.roles.new()
-          role.actor = actor['name']
+          role.actor = actor_name
       else:
         artists = tags["\xa9ART"][0]
         if len(artists) > 0:
